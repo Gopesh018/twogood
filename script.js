1ff74ee22e7336d8413f8e4741f84c1671d8e52f
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-06-22 18:14:46 +0530

    cursor correction

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function loco() {
 
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector(".main"),
-    smooth: true
+    smooth: true,
   });
   // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
   locoScroll.on("scroll", ScrollTrigger.update);
@@ -13,21 +13,29 @@ function loco() {
   // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
   ScrollTrigger.scrollerProxy(".main", {
     scrollTop(value) {
-      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
     }, // we don't have to define a scrollLeft because we're only scrolling vertically.
     getBoundingClientRect() {
-      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
     },
     // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
   });
 
-  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
   // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
   ScrollTrigger.refresh();
-
 }
 
 loco();
@@ -38,51 +46,54 @@ function page1Animation() {
     delay: 1,
     duration: 1,
     opacity: 0,
-    stagger: .2
-  })
-  gsap.from('.page1 .video-box video', {
+    stagger: 0.2,
+  });
+  gsap.from(".page1 .video-box video", {
     scale: 0.5,
     delay: 1.5,
-    y: 100
-  })
-
-  document.querySelector('.page1>.video-box').addEventListener('mousemove',(details) => {
-    gsap.to('.play-button',{
-      left:details.x,
-      top:details.y,
-      transform: "translate(-50%,-50%) scale(1)"
-    });
+    y: 100,
   });
 
-  document.querySelector('.page1>.video-box').addEventListener('mouseleave',(details) => {
-    gsap.to('.play-button',{
-      transform: "translate(-50%,-50%) scale(0)"
+  document
+    .querySelector(".page1>.video-box")
+    .addEventListener("mousemove", (details) => {
+      gsap.to(".play-button", {
+        left: details.x,
+        top: details.y,
+        transform: "translate(-50%,-50%) scale(1)",
+      });
     });
-  });
+
+  document
+    .querySelector(".page1>.video-box")
+    .addEventListener("mouseleave", (details) => {
+      gsap.to(".play-button", {
+        transform: "translate(-50%,-50%) scale(0)",
+      });
+    });
 }
 
 page1Animation();
 
 function cursorAnimation() {
-  document.addEventListener('mousemove', (dets) => {
-    gsap.to('.cursor', {
-      left: dets.x,
-      top: dets.y
-    })
-  })
+  document.addEventListener("mousemove", (dets) => {
+    gsap.to(".cursor", {
+      left: dets.clientX - 10 + "px",
+      top: dets.clientY - 10 + "px",
+    });
+  });
 
-  document.querySelectorAll('.page3-elem .elems img').forEach((i) => {
-    i.addEventListener('mousemove', () => {
-      gsap.to('.cursor', {
-        scale: 1
+  document.querySelectorAll(".page3-elem .elems img").forEach((i) => {
+    i.addEventListener("mousemove", () => {
+      gsap.to(".cursor", {
+        scale: 1,
       });
     });
-    i.addEventListener('mouseleave', () => {
-      gsap.to('.cursor', {
-        scale: 0
+    i.addEventListener("mouseleave", () => {
+      gsap.to(".cursor", {
+        scale: 0,
       });
     });
   });
-}cursorAnimation();
-
-
+}
+cursorAnimation();
